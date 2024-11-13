800dcf70c70b389798557724a944962f92a7adff
Austin Mubaya <bsc-inf-09-21@unima.ac.mw>
2024-11-13 00:22:25 +0000

    productcards

--- src/components/AddToCart/cartHandler.js
+++ src/components/AddToCart/cartHandler.js
@@ -1,18 +1,23 @@
 import { useState } from 'react';
-import vegetables from "./productImage1.jpeg";
-import tractor from "./tructor.jpeg";
-import wpump from "./water pump.jpeg";
+import vegetables from "./vegtables.jpg";
+import tractor from "./truck.jpg";
+import watapump from "./waterpump.jpg";
+import chickens from "./chickens.jpg";
+import maizes from "./maizemill.jpg";
+import milk from "./milky.jpg";
+import pest from "./pestcide.jpg";
+import cow from "./cow.jpg";
 
 const productsData = [
     { id: 1, name: 'vegetables', price: 2000, image: vegetables, description: 'fresh and clean vetables. MWK 2000 per killogram' },
     { id: 2, name: 'tractor', price: 500000.00, image: tractor, description: 'durable and versatile tructor. MWK 200000 each' },
-    { id: 3, name: 'Water pump', price: 200000.99, image: wpump, description: 'Duracbles water pump.' },
-    { id: 4, name: 'Water pump', price: 200000.99, image: wpump, description: 'Duracbles water pump.' },
-    { id: 5, name: 'Water pump', price: 200000.99, image: wpump, description: 'Duracbles water pump.' },
-    { id: 6, name: 'tractor', price: 5000000.00, image: tractor, description: 'durable and versatile tructor. MWK 200000 each' },
-    { id: 7, name: 'tractor', price: 5000000.00, image: tractor, description: 'durable and versatile tructor. MWK 200000 each' },
-    { id: 8, name: 'tractor', price: 5000000.00, image: tractor, description: 'durable and versatile tructor. MWK 200000 each' },
-    { id: 9, name: 'tractor', price: 5000000.00, image: tractor, description: 'durable and versatile tructor. MWK 200000 each' },
+    { id: 3, name: 'Water Pump', price: 350000.99, image: watapump, description: 'Duracbles water pump.' },
+    { id: 4, name: 'Chicken', price: 20000.99, image: chickens, description: 'Duracbles water pump.' },
+    { id: 5, name: 'Maize', price: 200000.99, image: maizes, description: 'Maize of Best quality.' },
+    { id: 6, name: 'Daily Products', price: 5000.00, image: milk, description: 'Milk of best Taste and Better Prices. MWK 5000 each' },
+    { id: 7, name: 'Pestcides & Herbicides', price: 4500.00, image: pest, description: 'Chemicals That Boost Fertility and Cutivation. MWK 4500 each' },
+    { id: 8, name: 'Tractors', price: 2000000.00, image: tractor, description: 'durable and versatile tractor with 4yrs Warrant. MWK 2000000 each' },
+    { id: 9, name: 'Cattle', price: 350000.00, image: cow, description: 'Healthy Cattle Multiple Breeds, Sex & Age . MWK 200000 each' },
 ];
 
 const useCartHandler = () => {

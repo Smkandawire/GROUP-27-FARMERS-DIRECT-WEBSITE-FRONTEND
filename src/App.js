d96ffb8dc7942f82d86407627d96575ceff33869
shaldremkandawire <shadreckmkandawire138@gmail.com>
2024-11-25 19:37:53 +0200

    renders, fixes on homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from "./components/Header/Header";
 import Home from "./components/Homepage/Home";
 import AboutUs from "./components/AboutUsPage/AboutUs";
@@ -18,34 +18,46 @@ import UserProfile from "./components/Userdashboard/UserProfile";
 import OrderManagement from "./components/Userdashboard/OrderManagement";
 
 function App() {
-  const { cart,addToCart, updateQuantity, removeFromCart } = useCartHandler();
+  const { cart, addToCart, updateQuantity, removeFromCart } = useCartHandler();
+  const location = useLocation();
+
+  // Define the paths where the header and footer should not be shown
+  const noHeaderPaths = ['/LoginPage', '/SignUp', '/payment','/Advertise'];
+  const noFooterPaths = ['/Checkout'];
 
   return (
-
-   <div>
-    <Router>
-         <Header/> 
     <div>
-      <Routes>
-         <Route path ="/"element={<Home/>}/>
-         <Route path ="AboutUs"element={<AboutUs/>}/>
-         <Route path="/Services" element={<Services products={cart.products} addToCart={addToCart} cartItemCount={cart.items.length} />} />
-         <Route path ="ContactUs"element={<ContactUs/>}/>
-         <Route path ="LoginPage"element={<LoginPage/>}/>
-         <Route path="/Checkout" element={<Checkout cart={cart.items} updateQuantity={updateQuantity} removeFromCart={removeFromCart} />} />
-         <Route path ="SignUp"element={<SignUp/>}/>
-         <Route path="Payment" element ={<Payment/>}/>
-         <Route path="/product/:productId" element={<ProductDetails products={cart.products} />} />
-         <Route path ="DescriptionPage"element={<DescriptionPage/>}/>
-         <Route path="Advertise" element ={<Advertise/>}/>
-         <Route path="OrderManagement" element ={<OrderManagement/>}/>
-         <Route path="UserProfile" element ={< UserProfile/>}/>
-      </Routes>
+      {/* Conditionally render the header based on the current path */}
+      {!noHeaderPaths.includes(location.pathname) && <Header />}
+      
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/Services" element={<Services products={cart.products} addToCart={addToCart} cartItemCount={cart.items.length} />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/LoginPage" element={<LoginPage />} />
+          <Route path="/Checkout" element={<Checkout cart={cart.items} updateQuantity={updateQuantity} removeFromCart={removeFromCart} />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Payment" element={<Payment />} />
+          <Route path="/product/:productId" element={<ProductDetails products={cart.products} />} />
+          <Route path="/DescriptionPage" element={<DescriptionPage />} />
+          <Route path="/Advertise" element={<Advertise />} />
+          <Route path="/OrderManagement" element={<OrderManagement />} />
+          <Route path="/UserProfile" element={<UserProfile />} />
+        </Routes>
+      </div>
+      
+      {/* Conditionally render the footer based on the current path */}
+      {!noFooterPaths.includes(location.pathname) && <Footer />}
     </div>
-      <Footer/>
-    </Router>  
-   </div>
   );
 }
 
-export default App;
+const AppWrapper = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default AppWrapper;
